2007b15c4feb9d46d0e9b52842ba0993399a8621
PULIPALPULA SIDDARTHA GOUD <psiddarthagoud9@gmail.com>
2025-08-24 23:55:52 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,8 @@ import Header from "../components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import Link from "next/link";
-const inter = Inter({ subset: ["latin"] });
+
+const inter = Inter({ subsets: ["latin"] }); // 👈 fixed here
 
 export const metadata = {
   title: "Aura",
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className}`}>
+        <body className={inter.className}>
           {/* header */}
           <Header />
           <main className="min-h-screen">{children}</main>
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
           {/* footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Developed by Sid siddartha</p>
+              <p>Developed by Sid Siddartha</p>
             </div>
           </footer>
         </body>
