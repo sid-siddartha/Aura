8a201db6baf6c1b7648b29a1fec916743b1071a7
PULIPALPULA SIDDARTHA GOUD <psiddarthagoud9@gmail.com>
2025-08-25 19:28:24 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -53,7 +53,7 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.webp"
+              src="/hero-banner.webp"
               width={1280}
               height={720}
               alt="Dashboard Preview"
